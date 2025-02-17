5e24b7404a2e7d4ed7d7c5b4de9c1701e82b16e1
tutatduy0050KHDL16A2 <117963880+tuduy0050@users.noreply.github.com>
2025-02-17 22:55:43 +0700

    Add files via upload

--- Chương 1/BTVN.sql
+++ Chương 1/BTVN.sql
@@ -1,5 +1,7 @@
-﻿DROP TABLE IF EXISTS NhanVien;
+﻿-- Xóa bảng "NhanVien" nếu tồn tại
+DROP TABLE IF EXISTS NhanVien;
 
+-- Tạo bảng "NhanVien"
 CREATE TABLE NhanVien (
     MaNV INT PRIMARY KEY,
     HoTen VARCHAR(50),
@@ -7,6 +9,7 @@ CREATE TABLE NhanVien (
     PhongBan VARCHAR(50)
 );
 
+-- Chèn các bản ghi nhân viên
 INSERT INTO NhanVien (MaNV, HoTen, Tuoi, PhongBan) VALUES
 (1, 'Nguyen Van A', 30, 'Ke Toan'),
 (2, 'Tran Thi B', 25, 'Nhan Su'),
@@ -23,20 +26,32 @@ INSERT INTO NhanVien (MaNV, HoTen, Tuoi, PhongBan) VALUES
 (13, 'Tran Thi M', 26, 'Marketing'),
 (14, 'Pham Van N', 30, 'Nhan Su'),
 (15, 'Hoang Thi O', 27, 'IT');
+
+-- Truy vấn tất cả thông tin từ bảng "NhanVien"
 SELECT * FROM NhanVien;
+
+-- Truy vấn tên và tuổi của các nhân viên trong phòng IT
 SELECT HoTen, Tuoi FROM NhanVien WHERE PhongBan = 'IT';
+
+-- Tìm các nhân viên lớn hơn 25 tuổi
 SELECT * FROM NhanVien WHERE Tuoi > 25;
+
+-- Xác định nhân viên lớn tuổi nhất trong mỗi phòng ban
 SELECT PhongBan, HoTen, MAX(Tuoi) AS Tuoi
 FROM NhanVien
 GROUP BY PhongBan, HoTen
 ORDER BY PhongBan;
+
+-- Thay đổi thông tin phòng ban của nhân viên "Le Van C" sang "Marketing"
 UPDATE NhanVien
 SET PhongBan = 'Marketing'
 WHERE HoTen = 'Le Van C';
+
+-- Xóa nhân viên có MaNV = 2
 DELETE FROM NhanVien
 WHERE MaNV = 2;
 
+-- Hiển thị số lượng nhân viên trong mỗi phòng ban
 SELECT PhongBan, COUNT(*) AS SoNhanVien
 FROM NhanVien
 GROUP BY PhongBan;
-
