aa4c6b586031a04cf4f23adc2cefa285eebae90e
tutatduy0050KHDL16A2 <117963880+tuduy0050@users.noreply.github.com>
2025-02-17 20:37:12 +0700

    Add files via upload

---
+++ Chương 1/BTVN.sql
@@ -0,0 +1,42 @@
+﻿DROP TABLE IF EXISTS NhanVien;
+
+CREATE TABLE NhanVien (
+    MaNV INT PRIMARY KEY,
+    HoTen VARCHAR(50),
+    Tuoi INT,
+    PhongBan VARCHAR(50)
+);
+
+INSERT INTO NhanVien (MaNV, HoTen, Tuoi, PhongBan) VALUES
+(1, 'Nguyen Van A', 30, 'Ke Toan'),
+(2, 'Tran Thi B', 25, 'Nhan Su'),
+(3, 'Le Van C', 28, 'IT'),
+(4, 'Pham Thi D', 32, 'Ke Toan'),
+(5, 'Vu Van E', 26, 'IT'),
+(6, 'Nguyen Thi F', 29, 'Marketing'),
+(7, 'Le Thi G', 27, 'Nhan Su'),
+(8, 'Hoang Van H', 35, 'Ke Toan'),
+(9, 'Pham Van I', 33, 'Marketing'),
+(10, 'Tran Van J', 24, 'IT'),
+(11, 'Dang Thi K', 31, 'Nhan Su'),
+(12, 'Nguyen Van L', 28, 'Ke Toan'),
+(13, 'Tran Thi M', 26, 'Marketing'),
+(14, 'Pham Van N', 30, 'Nhan Su'),
+(15, 'Hoang Thi O', 27, 'IT');
+SELECT * FROM NhanVien;
+SELECT HoTen, Tuoi FROM NhanVien WHERE PhongBan = 'IT';
+SELECT * FROM NhanVien WHERE Tuoi > 25;
+SELECT PhongBan, HoTen, MAX(Tuoi) AS Tuoi
+FROM NhanVien
+GROUP BY PhongBan, HoTen
+ORDER BY PhongBan;
+UPDATE NhanVien
+SET PhongBan = 'Marketing'
+WHERE HoTen = 'Le Van C';
+DELETE FROM NhanVien
+WHERE MaNV = 2;
+
+SELECT PhongBan, COUNT(*) AS SoNhanVien
+FROM NhanVien
+GROUP BY PhongBan;
+
